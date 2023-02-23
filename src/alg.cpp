559483d8e5bf4e442aeecacad87b39b0a2ff77c3
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-02-23 11:24:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,43 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t c = 0;
+    for (uint64_t i = 2; i < value; i++) {
+      if (value % i == 0) {			
+        c++;
+      }
+    }
+    if (c == 0)
+      return true;
+    else
+      return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t count = 0;
+	for (uint64_t i = 2; i <= 10000; i++) {
+		if (checkPrime(i) == true) {
+			count++;
+		}
+		if (count == n) {
+			return i;			
+		}
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  for (uint64_t i = value; i <= value + 1000; i++) {
+		if (checkPrime(i) == true)
+			return i;
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+	for (uint64_t i = 2; i < hbound; i++) {
+		if (checkPrime(i) == true)
+			sum += i;
+	}
+	return sum;
 }
+
