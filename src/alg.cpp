09be5fb97a0af0013bb80f7ca49912e2cf730e77
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-02-25 11:28:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,11 +7,11 @@ for (uint64_t i = 2; i < value; i++) {
 if (value % i == 0) {
 c++;
 }
-}
 if (c == 0)
 return true;
 else
 return false;
+}
 }
 uint64_t nPrime(uint64_t n) {
 uint64_t count = 0;
@@ -29,12 +29,12 @@ for (uint64_t i = value; i <= value + 1000; i++) {
 if (checkPrime(i) == true)
 return i;
 }
+}
 uint64_t sumPrime(uint64_t hbound) {
 uint64_t sum = 0;
 for (uint64_t i = 2; i < hbound; i++) {
 if (checkPrime(i) == true)
 sum += i;
-}
 }
 return sum;
 }
