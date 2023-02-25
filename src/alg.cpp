9564834cbcd13961e5cf182c65020005186f4def
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-02-25 22:40:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,14 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-
+#include <cmath>
 #include "alg.h"
 bool checkPrime(uint64_t value) {
   uint64_t c = 0;
-  for (uint64_t i = 2; i < sqrt(value); i++) {
-    if (value % i == 0) {
-      return false;
-    }
-  return true;
-}
+  for (uint64_t i = 2; i < (value); i++) {
+    if (value % i == 0) return false;
+  }
+    return true;
+  }
 uint64_t nPrime(uint64_t n) {
   uint64_t count = 0;
   uint64_t d = 0;
